d2f8f4f8fedbb93ecec5f88b5d7bc68bd3a5a32f
a01798102 <arturomontes481@gmail.com>
2023-10-25 10:57:22 -0600

    added reading files functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{fs, task::Context};
+#[derive(Debug)]
+#[allow(dead_code)]
+enum FileType {
+    INPUT,
+    EXPECTED,
+    CODE,
+    OUTPUT,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct File {
+    file_type: FileType,
+    content: String,
+}
+impl File {
+    fn new(file_type: FileType, file: &str) -> Self {
+        File {
+            file_type,
+            content: fs::read_to_string(file).expect("Not found!!!"),
+        }
+    }
+}
+
 fn main() {
-    println!("Helro, world!");
+    let input_values = File::new(FileType::INPUT, "../tests/a.in");
+    let expected_output = File::new(FileType::EXPECTED, "../tests/expected");
+    println!("{:}", &input_values.content);
+    println!("{:}", &expected_output.content);
 }
