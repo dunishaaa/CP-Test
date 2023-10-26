04996c7b5cf2bf0673f952315692eb9ac659b12a
a01798102 <arturomontes481@gmail.com>
2023-10-25 21:01:40 -0600

    rediricting stdio of files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs, task::Context};
+use std::{
+    fs,
+    process::{self, Command, Stdio},
+};
 #[derive(Debug)]
 #[allow(dead_code)]
 enum FileType {
@@ -27,5 +30,34 @@ fn main() {
     let input_values = File::new(FileType::INPUT, "../tests/a.in");
     let expected_output = File::new(FileType::EXPECTED, "../tests/expected");
     println!("{:}", &input_values.content);
-    println!("{:}", &expected_output.content);
+    //    println!("{:}", &expected_output.content);
+    let status = Command::new("g++")
+        .arg("../tests/cowSign.cpp")
+        .arg("-o")
+        .arg("../tests/main")
+        .status()
+        .expect("Valio burguer");
+
+    if status.success() {
+        println!("Compilation succesful. Now testing...");
+        let input = Command::new("echo")
+            .arg("1 9")
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to create process");
+        let echo_out = input.stdout.expect("Failed to open process");
+
+        let test = Command::new("./../tests/main")
+            .stdin(Stdio::from(echo_out))
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Hijole");
+        let ans = test.wait_with_output().expect("kk");
+        //        let idk = ans.stdout;
+        println!("{:?}", ans);
+    //        assert_eq!("1 2 3 4 5 6 7 8 \n", ans.stdout);
+    } else {
+        println!("Compilation unsuccesful:(((");
+    }
+    //    compile.spawn();
 }
